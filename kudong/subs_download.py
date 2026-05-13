fccead0f89c3f1cd9bdb16a820d1ab25e7bc64ae
dhku <dhku@naver.com>
2026-05-13 19:58:57 +0900

    fix: 리다이렉션 url 주소 대응

--- kudong/subs_download.py
+++ kudong/subs_download.py
@@ -49,19 +49,25 @@ download_progress_length = 0;
 console_output = ""
 log_path = ""
 
-def init_paths():
-    global outpath, log_path
-    outpath = os.path.abspath('.') + "/"
-    log_path = os.path.abspath('.') + "/log/"
-
 p_extension = re.compile(r"^.*\.(zip|ass|smi|7z)$", re.IGNORECASE)
 regrex1 = re.compile(r".*(naver).*")
 regrex2 = re.compile(r".*(blogspot).*")
 regrex3 = re.compile(r".*(tistory).*")
 
+p_google = re.compile(r"(.*(https://drive.google.com/file/d/).*)")
+p_google_2_1 = re.compile(r"(.*(https://docs.google.com/uc).*)")
+p_google_2_2 = re.compile(r"(.*(https://drive.google.com/uc).*)")
+p_google_3 = re.compile(r"(.*(https://drive.usercontent.google.com/download).*)")
+erulabo = re.compile(r"(.*(https://erulabo.com/file).*)")
+
 thread_lock = threading.Lock()
 isRunning = False
 quitSignal = False
+
+def init_paths():
+    global outpath, log_path
+    outpath = os.path.abspath('.') + "/"
+    log_path = os.path.abspath('.') + "/log/"
 
 def download(url, file_name = None):
     with open(file_name, "wb") as file:  
@@ -1032,19 +1038,100 @@ def download_website(url,callback):
     soup = BeautifulSoup(url_source, 'html.parser')
     temps = soup.find('div')
 
-    links = temps.find_all("a")
-
-    p_google = re.compile(r"(.*(https://drive.google.com/file/d/).*)")
-    p_google_2_1 = re.compile(r"(.*(https://docs.google.com/uc).*)")
-    p_google_2_2 = re.compile(r"(.*(https://drive.google.com/uc).*)")
-    p_google_3 = re.compile(r"(.*(https://drive.usercontent.google.com/download).*)")
-    erulabo = re.compile(r"(.*(https://erulabo.com/file).*)")
-
     isDownloaded = 0;
+    isDownloaded = find_blog_standard(temps,callback)
+    isDownloaded = find_blog_1(temps,url,callback)
+
+    if isDownloaded == 0:
+        isDownloadError = 1;
+
+def find_blog_standard(temps,callback):
+    global isDownloadError, download_progress_count, download_progress_length
+
+    links = temps.find_all("a")
 
     for a in links:
         each_file = a.attrs['href']
         #print_log("href = "+each_file)
+
+        try:
+            each_file = each_file.replace('&amp;','&');
+
+            if bool(p_google_2_1.match(each_file)):
+                start_index = each_file.find("&id=") + 4;
+                end_index =  each_file.rfind("&confirm");
+                each_file = "https://drive.google.com/file/d/" + each_file[start_index:end_index] + "/view"
+
+            if bool(p_google_2_2.match(each_file)):
+                start_index = each_file.find("&id=") + 4;
+                end_index =  len(each_file);
+                each_file = "https://drive.google.com/file/d/" + each_file[start_index:end_index] + "/view"
+
+            if bool(p_google_3.match(each_file)):
+                start_index = each_file.find("?id=") + 4;
+                end_index =  each_file.rfind("&export");
+                each_file = "https://drive.google.com/file/d/" + each_file[start_index:end_index] + "/view"
+
+            # 구글 드라이브 주소가 검출되었을때
+            if bool(p_google.match(each_file)):
+
+                start_index = each_file.find("/d/") + 3;
+                end_index =  each_file.rfind("/view");
+
+                key = each_file[start_index:end_index]
+                each_file = "https://drive.google.com/uc?id="+key
+
+                remotefile = urlopen(each_file)
+                fileName = remotefile.headers.get_filename();
+
+                if fileName is not None:
+                    fileName = fileName.encode('ISO-8859-1').decode('UTF-8');
+                else:
+                    parsed_url = urlparse(each_file)
+                    fileName = os.path.basename(parsed_url.path)
+                    fileName = unquote(fileName)
+
+                path = outpath + smiDir
+
+                if fileName == "uc":
+                    fileName = gdrive.get_file_name(each_file)
+
+                #print_log(fileName);
+
+                if(not p_extension.match(fileName)):
+                    download_progress_count += 1
+                    callback(download_progress_count,download_progress_length)
+                    continue;
+
+                print_log("[=] 다운로드 시작 => "+ fileName)
+
+                if not os.path.exists(path):
+                    os.makedirs(path)
+
+                gdrive.download(each_file, path + fileName, quiet=False)
+                print_log("[+] 파일 다운로드가 완료 되었습니다. ")
+                    
+                download_progress_count += 1
+                callback(download_progress_count,download_progress_length)
+                return 1;
+        except urllib.error.HTTPError as e:
+            print_log("[=] 해당 URL은 스킵되었습니다. : %s" % e)
+            download_progress_count += 1
+            return 0;
+        except Exception as e:
+            print_log("[-] Error : %s" % e)
+            print_log(traceback.format_exc())
+            download_progress_count += 1
+            return 0;
+
+def find_blog_1(temps,url,callback):
+    global isDownloadError, download_progress_count, download_progress_length
+
+    links = temps.find_all("button", attrs={"data-file-url": True})
+
+    for a in links:
+        each_file = "https://erulabo.com" + a.attrs['data-file-url']
+        #print("data-file-url = "+each_file)
 
         try:
             each_file = each_file.replace('&amp;','&');
@@ -1144,20 +1231,19 @@ def download_website(url,callback):
                 gdrive.download(each_file, path + fileName, quiet=False)
                 print_log("[+] 파일 다운로드가 완료 되었습니다. ")
                     
-                isDownloaded = 1;
                 download_progress_count += 1
                 callback(download_progress_count,download_progress_length)
-
+                return 1;
+    
         except urllib.error.HTTPError as e:
             print_log("[=] 해당 URL은 스킵되었습니다. : %s" % e)
-            download_progress_count += 1 
+            download_progress_count += 1
+            return 0;
         except Exception as e:
             print_log("[-] Error : %s" % e)
             print_log(traceback.format_exc())
             download_progress_count += 1
-
-    if isDownloaded == 0:
-        isDownloadError = 1;
+            return 0;
 
 def download_count_website(url):
 
@@ -1171,9 +1257,6 @@ def download_count_website(url):
 
     links = temps.find_all("a")
 
-    p_google = re.compile(r"(.*(https://drive.google.com/file/d/).*)")
-    erulabo = re.compile(r"(.*(https://erulabo.com/file).*)")
-
     download_count = 0;
 
     for a in links:
@@ -1182,11 +1265,25 @@ def download_count_website(url):
         try:
             each_file = each_file.replace('&amp;','&');
             # 구글 드라이브 주소가 검출되었을때
-            if bool(p_google.match(each_file)) or bool(erulabo.match(each_file)):
+            if bool(p_google.match(each_file)):
                 print_log("[+] 구글 드라이브 주소가 검출되었습니다.")
                 download_count += 1
         except Exception as e:
-            download_count += 0
+            download_count = 0
+
+    links = temps.find_all("button", attrs={"data-file-url": True})
+
+    for a in links:
+        each_file = "https://erulabo.com" + a.attrs['data-file-url']
+        try:
+            each_file = each_file.replace('&amp;','&');
+            # 구글 드라이브 주소가 검출되었을때
+            if bool(erulabo.match(each_file)):
+                print_log("[+] 구글 드라이브 주소가 검출되었습니다.")
+                download_count += 1
+        except Exception as e:
+            download_count = 0
+
     return download_count    
 
 def get_url_source_website(url):
